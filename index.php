e49455383b253ea334f9a7c08371af8cebaeba13
ingr.in <social@ingr.in>
2025-07-28 19:05:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
-require_once 'includes/config.php';
-require_once 'includes/functions.php';
 
-$page = isset($_GET['page']) ? $_GET['page'] : 'home';
-$allowed_pages = ['home', 'about', 'gallery', 'contact', 'admin'];
-
-if (!in_array($page, $allowed_pages)) {
-    $page = 'home';
-}
-
-require_once 'includes/header.php';
-require_once "pages/$page.php";
-require_once 'includes/footer.php';
-?>
